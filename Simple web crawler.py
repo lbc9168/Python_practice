2f1fda96bfc89852907aac25c78c2d7caaa3a971
Bingcai Liu <liu.3444.osu@gmail.com>
2015-12-31 18:44:32 -0500

    print information in each webpage

--- Simple web crawler.py
+++ Simple web crawler.py
@@ -9,11 +9,21 @@ def stepashka_spider(max_pages):
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text, "html.parser")
         for resoult in soup.findAll("div", {"class": "header col-xs-12"}):
-             a_tag = resoult.a
+            a_tag = resoult.a
 
-             print(a_tag["href"])
-             print(a_tag["title"])
+            print(a_tag["href"])
+            print(a_tag["title"])
+            get_single_item_data(a_tag["href"])
 
         page += 1
 
-stepashka_spider(1)
+
+def get_single_item_data(item_url):
+    source_code = requests.get(item_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "html.parser")
+    for item_price in soup.findAll('span', {"class": "car-price"}):
+        print(item_price.string)
+
+
+stepashka_spider(2)
